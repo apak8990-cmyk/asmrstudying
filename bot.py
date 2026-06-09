2a1cb11f62bc266644a5a23b4512acc8d35eaf5a
apak8990-cmyk <apak8990@gmail.com>
2026-06-09 12:03:34 +0300

    Updated all study worlds

--- bot.py
+++ bot.py
@@ -1,35 +1,37 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message, WebAppInfo
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.filters import CommandStart
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+import asyncio
 
 TOKEN = "8730112261:AAF4MHg6lCM1DIlMoWGKXI127mLImuKE2gA"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
 @dp.message(CommandStart())
 async def start(message: Message):
-    kb = InlineKeyboardBuilder()
 
-    kb.button(
-        text="🎧 Open Study Room",
-        web_app=WebAppInfo(
-            url="https://YOUR-WEBSITE-URL"
-        )
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="✨ Start Study Session",
+                    web_app=WebAppInfo(
+                        url="https://apak8990-cmyk.github.io/asmrstudying/website/"
+                    )
+                )
+            ]
+        ]
     )
 
     await message.answer(
-        "Welcome to ASMR Studying!",
-        reply_markup=kb.as_markup()
+        "Choose your ASMR study world ✨",
+        reply_markup=keyboard
     )
-
 
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
